ace42e418e2f13c622d5e4b423e30a4635e36f67
Praveen <praveenthakur0962@gmail.com>
2024-01-06 23:56:54 +0530

    temp

--- bootdevtool-1/src/main/java/com/api/book/Bootdevtool1Application.java
+++ bootdevtool-1/src/main/java/com/api/book/Bootdevtool1Application.java
@@ -8,6 +8,7 @@ public class Bootdevtool1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bootdevtool1Application.class, args);
+		System.out.println("Prveen");
 	}
 
 }
